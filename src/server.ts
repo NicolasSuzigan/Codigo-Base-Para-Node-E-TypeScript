de520e13d39a695ef08a410a9494c9eafc0900f9
Nícolas Suzigan <nicolas_suzigan@hotmail.com>
2020-04-18 16:38:32 -0300

    add eslint no node

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express, { request } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>{
-  return response.json({ message: 'hello nicoleta!'});
-});
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333')
+  console.log('Server started on port 3333');
 });
